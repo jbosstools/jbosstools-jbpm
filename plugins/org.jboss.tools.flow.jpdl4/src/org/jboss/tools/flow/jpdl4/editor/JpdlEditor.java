3e114efba3f88ce7be235e94420a03409d568e2c
Koen Aers <koen.aers@jboss.com>
2008-10-13 18:26:06 +0000

    jpdl initial serialization experiment
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@10808 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/editor/JpdlEditor.java
+++ plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/editor/JpdlEditor.java
@@ -3,12 +3,18 @@ package org.jboss.tools.flow.jpdl4.editor;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
 
 import org.eclipse.gef.EditPartFactory;
 import org.eclipse.gef.palette.PaletteRoot;
 import org.jboss.tools.flow.common.editor.GenericModelEditor;
 import org.jboss.tools.flow.common.editpart.DefaultEditPartFactory;
+import org.jboss.tools.flow.common.model.Element;
 import org.jboss.tools.flow.common.registry.ElementRegistry;
+import org.jboss.tools.flow.common.wrapper.ConnectionWrapper;
+import org.jboss.tools.flow.common.wrapper.ContainerWrapper;
+import org.jboss.tools.flow.common.wrapper.NodeWrapper;
+import org.jboss.tools.flow.common.wrapper.Wrapper;
 
 public class JpdlEditor extends GenericModelEditor {
 
@@ -24,8 +30,34 @@ public class JpdlEditor extends GenericModelEditor {
         return ElementRegistry.createWrapper("org.jboss.tools.flow.jpdl4.process");
     }
     
+    private void write(Wrapper wrapper, OutputStream os) {
+    	Object object = wrapper.getElement();
+        if (object instanceof Element) {
+        	Element element = (Element)object;
+        	String xml = (String)element.getMetaData("xml");
+            System.out.println(xml);
+        }
+        if (wrapper instanceof ContainerWrapper) {
+        	ContainerWrapper containerWrapper = (ContainerWrapper)wrapper;
+            List<NodeWrapper> children = containerWrapper.getElements();
+            for (NodeWrapper nodeWrapper : children) {
+    			write(nodeWrapper, os);
+    		}
+        }
+        if (wrapper instanceof NodeWrapper) {
+        	NodeWrapper nodeWrapper = (NodeWrapper)wrapper;
+        	List<ConnectionWrapper> children = nodeWrapper.getOutgoingConnections();
+        	for (ConnectionWrapper connectionWrapper : children) {
+        		write(connectionWrapper, os);
+        	}
+        }
+    }
+    
     protected void writeModel(OutputStream os) throws IOException {
-        // TODO
+        Object object = getModel();
+        if (object instanceof Wrapper) {
+        	write((Wrapper)object, os);
+        }
     }
     
     protected void createModel(InputStream is) {
