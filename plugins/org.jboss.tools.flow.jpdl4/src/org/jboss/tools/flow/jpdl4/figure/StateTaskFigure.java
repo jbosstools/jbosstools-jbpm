04dcf795f0e3aa524a2335a0c647ecac78a612b2
Koen Aers <koen.aers@jboss.com>
2008-11-16 22:19:45 +0000

    figures for start events and states
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@11827 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/figure/StateTaskFigure.java
@@ -0,0 +1,7 @@
+package org.jboss.tools.flow.jpdl4.figure;
+
+import org.jboss.tools.flow.common.figure.RoundedRectangleElementFigure;
+
+public class StateTaskFigure extends RoundedRectangleElementFigure {
+
+}
