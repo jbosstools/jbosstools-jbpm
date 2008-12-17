1e04291fa00bbb8ddc8a4facaa30825e98664fa7
Koen Aers <koen.aers@jboss.com>
2008-12-17 00:26:45 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@12632 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/figure/StartEventFigure.java
+++ plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/figure/StartEventFigure.java
@@ -5,7 +5,7 @@ import org.eclipse.swt.graphics.Image;
 import org.jboss.tools.flow.jpdl4.Activator;
 
 
-public class StartEventFigure extends EventFigure { // EventFigure {
+public class StartEventFigure extends EventFigure { 
     
     private static final Image icon = ImageDescriptor.createFromURL(
 		Activator.getDefault().getBundle().getEntry("icons/48/start_event_empty.png")).createImage();;
