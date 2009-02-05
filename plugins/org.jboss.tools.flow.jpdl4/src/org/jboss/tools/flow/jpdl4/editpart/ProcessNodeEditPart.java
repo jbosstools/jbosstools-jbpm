5e9f906368c13cac079f9422c55840efaf505e49
Koen Aers <koen.aers@jboss.com>
2009-02-05 23:16:30 +0000

    initial default name for connections
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@13497 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/editpart/ProcessNodeEditPart.java
+++ plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/editpart/ProcessNodeEditPart.java
@@ -1,9 +1,17 @@
 package org.jboss.tools.flow.jpdl4.editpart;
 
+import org.eclipse.gef.EditPolicy;
 import org.jboss.tools.flow.common.editpart.NodeEditPart;
+import org.jboss.tools.flow.jpdl4.policy.ProcessNodeGraphicalNodeEditPolicy;
 
 public class ProcessNodeEditPart extends NodeEditPart {
 	
+    protected void createEditPolicies() {
+    	super.createEditPolicies();
+        installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE, new ProcessNodeGraphicalNodeEditPolicy());
+   }
+    
+    // make performDirectEdit public
 	public void performDirectEdit() {
 		super.performDirectEdit();
 	}
