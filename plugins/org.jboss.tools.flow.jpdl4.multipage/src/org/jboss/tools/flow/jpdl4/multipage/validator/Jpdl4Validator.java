64d5cf335d166dcbbc270516bc7063d72cf5654f
Koen Aers <koen.aers@jboss.com>
2009-06-15 22:41:52 +0000

    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@15963 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.flow.jpdl4.multipage/src/org/jboss/tools/flow/jpdl4/multipage/validator/Jpdl4Validator.java
+++ plugins/org.jboss.tools.flow.jpdl4.multipage/src/org/jboss/tools/flow/jpdl4/multipage/validator/Jpdl4Validator.java
@@ -5,6 +5,10 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.wst.validation.AbstractValidator;
 import org.eclipse.wst.validation.ValidationResult;
 import org.eclipse.wst.validation.ValidationState;
+import org.jboss.tools.flow.jpdl4.Activator;
+import org.jboss.tools.jbpm.Constants;
+import org.jboss.tools.jbpm.preferences.JbpmInstallation;
+import org.jboss.tools.jbpm.preferences.PreferencesManager;
 
 public class Jpdl4Validator extends AbstractValidator {
 
@@ -14,6 +18,15 @@ public class Jpdl4Validator extends AbstractValidator {
             ValidationState state,
             IProgressMonitor monitor) {
 		System.out.println("validating...");
+		PreferencesManager manager = PreferencesManager.getPreferencesManager(Activator.getDefault());
+		if (manager == null) return super.validate(resource, kind, state, monitor);
+		String jbpmName = Activator.getDefault().getPluginPreferences().getString(Constants.JBPM_NAME);
+		if (jbpmName == null) return super.validate(resource, kind, state, monitor);
+		JbpmInstallation jbpmInstallation = manager.getJbpmInstallation(jbpmName);
+		if (jbpmInstallation == null) return super.validate(resource, kind, state, monitor);
+		String location = jbpmInstallation.location;
+		if (location == null) return super.validate(resource, kind, state, monitor);
+		System.out.println("location : " + location);
 //		Thread.currentThread().getContextClassLoader().
 		return super.validate(resource, kind, state, monitor);
 	}
