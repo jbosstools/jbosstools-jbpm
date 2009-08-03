d40e0a2e8bc4c5c63b82738fdac179bde5bf2d89
Koen Aers <koen.aers@jboss.com>
2009-08-03 09:46:40 +0000

    GPD-356: Designer creates duplicate handler nodes under decision nodes
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@16940 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/io/ExclusiveGatewaySerializer.java
+++ plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/io/ExclusiveGatewaySerializer.java
@@ -9,20 +9,26 @@ import org.jboss.tools.flow.common.wrapper.Wrapper;
 import org.jboss.tools.flow.jpdl4.model.ExclusiveGateway;
 
 class ExclusiveGatewaySerializer extends ProcessNodeSerializer {
+	
 	protected List<String> getAttributesToSave() {
 		List<String> result = super.getAttributesToSave();
 		result.add("expr");
 		result.add("lang");
 		return result;
 	}
+	
 	public void appendBody(StringBuffer buffer, Wrapper wrapper, int level) {
 		ExclusiveGateway exclusiveGateway = (ExclusiveGateway)wrapper.getElement();
 		String handler = exclusiveGateway.getHandler();
 		if (handler != null && !"".equals(handler)) {
-			buffer.append("\n");
-			appendPadding(buffer, level + 1);
-			buffer.append("<handler class=\"" + handler + "\" />");
+			appendHandler(buffer, wrapper, handler, level);
 		} 
 		super.appendBody(buffer, wrapper, level);
 	}
+	
+	private void appendHandler(StringBuffer buffer, Wrapper wrapper, String handler, int level) {
+		appendUnknownNodes("beforeHandlerNodes", buffer, wrapper, level);
+		buffer.append("<handler class=\"").append(handler).append("\" />");
+	}
+	
 }
