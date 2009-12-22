f00efb364186c2c5c1874a2b30fef339283af02d
Koen Aers <koen.aers@jboss.com>
2009-12-22 02:04:29 +0000

    Add possibility to specify the jpdl version to the new file wizard: GPD-383
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@19511 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/wizard/NewJpdl4FileWizardPage.java
+++ plugins/org.jboss.tools.flow.jpdl4/src/org/jboss/tools/flow/jpdl4/wizard/NewJpdl4FileWizardPage.java
@@ -4,9 +4,17 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 
 import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
 public class NewJpdl4FileWizardPage extends WizardNewFileCreationPage {
+	
+	private Combo combo;
 
 	public NewJpdl4FileWizardPage(IStructuredSelection selection) {
 		super("NewJpdl4FileWizardPage", selection);
@@ -23,9 +31,38 @@ public class NewJpdl4FileWizardPage extends WizardNewFileCreationPage {
     private String getInitialContentsAsString() {
     	StringBuffer buffer = new StringBuffer();
     	buffer.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n\n");
-    	buffer.append("<process xmlns=\"http://jbpm.org/4.0/jpdl\" name=\"");
+    	buffer.append("<process xmlns=\"http://jbpm.org/"+ combo.getText().trim() + "/jpdl\" name=\"");
     	buffer.append(getFileName().substring(0, getFileName().lastIndexOf(getFileExtension()) - 1));
     	buffer.append("\">\n</process>");
     	return buffer.toString();
     }
+    
+	protected void createAdvancedControls(Composite parent) {
+		createVersionControls(parent);
+		super.createAdvancedControls(parent);
+	}
+	
+	protected void createVersionControls(Composite parent) {
+		Composite versionControlsParent = new Composite(parent, SWT.NONE);
+		versionControlsParent.setFont(parent.getFont());
+		versionControlsParent.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		GridLayout layout = new GridLayout();
+		layout.numColumns = 2;
+		layout.marginHeight = 0;
+		layout.marginWidth = 0;
+		versionControlsParent.setLayout(layout);
+		
+		Label label = new Label(versionControlsParent, SWT.NONE);
+		label.setText("Version:   ");
+		label.setFont(parent.getFont());
+		combo = new Combo(versionControlsParent, SWT.BORDER | SWT.READ_ONLY);
+		combo.setFont(parent.getFont());
+		combo.setSize(50, combo.getSize().y);
+		combo.add("  4.3");
+		combo.add("  4.2");
+		combo.add("  4.1");
+		combo.add("  4.0");
+		combo.select(0);
+	}
+    
 }
