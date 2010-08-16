2aad9e49c36f65ef928218a11f90439ad8f8f938
Koen Aers <koen.aers@jboss.com>
2010-08-16 21:20:26 +0000

    JBIDE-6649
    git-svn-id: http://svn.jboss.org/repos/jbosstools/branches/jbosstools-3.1.x@24194 a97e2381-89e5-4abb-bab3-167db6db766c

---
+++ plugins/org.jbpm.gd.jpdl/src/org/jbpm/gd/jpdl/refactoring/DeleteProcessHandler.java
@@ -0,0 +1,13 @@
+package org.jbpm.gd.jpdl.refactoring;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class DeleteProcessHandler extends AbstractProcessHandler {
+
+	public Object execute(ExecutionEvent arg0) throws ExecutionException {
+		System.out.println("hello, delete world");
+		return null;
+	}
+
+}
---
+++ plugins/org.jbpm.gd.jpdl/src/org/jbpm/gd/jpdl/refactoring/MoveProcessHandler.java
@@ -0,0 +1,13 @@
+package org.jbpm.gd.jpdl.refactoring;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class MoveProcessHandler extends AbstractProcessHandler {
+
+	public Object execute(ExecutionEvent arg0) throws ExecutionException {
+		System.out.println("hello, move world");
+		return null;
+	}
+
+}
