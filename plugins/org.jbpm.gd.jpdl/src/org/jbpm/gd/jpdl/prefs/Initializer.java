300fd8836e7f2590b0a0696e71fcc85ce71b64d0
Denis Golovin <dgolovin@exadel.com>
2011-05-03 21:01:25 +0000

    https://issues.jboss.org/browse/JBDS-1646 jBPM upload URL is: "/jbpm-console/upload" but it should be "/gpd-deployer/upload" in JBDS 4 fix for preferences initialization using plugin_customization.ini
    git-svn-id: http://svn.jboss.org/repos/jbosstools/trunk@31046 a97e2381-89e5-4abb-bab3-167db6db766c

--- plugins/org.jbpm.gd.jpdl/src/org/jbpm/gd/jpdl/prefs/Initializer.java
+++ plugins/org.jbpm.gd.jpdl/src/org/jbpm/gd/jpdl/prefs/Initializer.java
@@ -1,20 +1,20 @@
 package org.jbpm.gd.jpdl.prefs;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.jface.preference.IPreferenceStore;
-import org.jbpm.gd.jpdl.Plugin;
+import org.eclipse.core.runtime.preferences.DefaultScope;
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
 public class Initializer extends AbstractPreferenceInitializer implements PreferencesConstants {
 
 	@Override
 	public void initializeDefaultPreferences() {
-		IPreferenceStore preferenceStore = Plugin.getDefault().getPreferenceStore();
-		preferenceStore.setValue(SERVER_NAME, "localhost");
-		preferenceStore.setValue(SERVER_PORT, "8080");
-		preferenceStore.setValue(SERVER_DEPLOYER, "/jbpm-console/upload");
-		preferenceStore.setValue(USE_CREDENTIALS, false);
-		preferenceStore.setValue(USER_NAME, "user name");
-		preferenceStore.setValue(PASSWORD, "password");
+		IEclipsePreferences preferenceStore = new DefaultScope().getNode("org.jbpm.gd.jpdl");
+		preferenceStore.put(SERVER_NAME, "localhost");
+		preferenceStore.put(SERVER_PORT, "8080");
+		preferenceStore.put(SERVER_DEPLOYER, "/jbpm-console/upload");
+		preferenceStore.putBoolean(USE_CREDENTIALS, false);
+		preferenceStore.put(USER_NAME, "user name");
+		preferenceStore.put(PASSWORD, "password");
 	}
 
 }
